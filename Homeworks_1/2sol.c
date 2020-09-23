9e0ae0892ad17806082909750f03340f3db753c8
rinatisk <70975579+rinatisk@users.noreply.github.com>
2020-09-23 21:51:16 +0300

    fix sol2

--- Homeworks_1/2sol.c
+++ Homeworks_1/2sol.c
@@ -1,43 +1,39 @@
 #include <stdio.h>
-
+#include "../library/commonUtils/numericOperations.h"
+#include <stdlib.h>
 int main()
 {
-    int m, n;
+    int m = 0;
+    int n = 0;
     scanf("%d%d", &m, &n);
     int x[m + n];
     for (int i = 0; i < m + n; ++i) {
         scanf("%d", &x[i]);
     }
-    int tmp = 0;
     int left = 0;
     int right = m - 1;
     while (left < right) {
-        tmp = x[left];
-        x[left] = x[right];
-        x[right] = tmp;
+        swap(right, left);
         right--;
         left++;
     }
     left = m;
     right = m + n - 1;
     while (left < right) {
-        tmp = x[left];
-        x[left] = x[right];
-        x[right] = tmp;
+        swap(right, left);
         right--;
         left++;
     }
     left = 0;
     right = m + n - 1;
     while (left < right) {
-        tmp = x[left];
-        x[left] = x[right];
-        x[right] = tmp;
+        swap(right, left);
         right--;
         left++;
     }
     for (int i = 0; i < m + n; ++i) {
         printf("%d%s", x[i], " ");
     }
+    free(x);
     return 0;
 }
