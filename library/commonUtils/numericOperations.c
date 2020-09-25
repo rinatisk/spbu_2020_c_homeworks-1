0570a8505e25a255675bd52fd016b831731dd26f
rinatisk <70975579+rinatisk@users.noreply.github.com>
2020-09-25 11:45:50 +0300

    fix sol 1

--- library/commonUtils/numericOperations.c
+++ library/commonUtils/numericOperations.c
@@ -1,5 +1,6 @@
 #include "numericOperations.h"
 #include <malloc.h>
+
 int min(int a, int b)
 {
     return a > b ? b : a;
@@ -9,18 +10,20 @@ int max(int a, int b)
 {
     return a > b ? a : b;
 }
-int ** dynamic_array_alloc(int N, int M)
+
+int **dynamic_array_alloc(int N, int M)
 {
-  int **A = (int **)malloc(N*sizeof(int *));
-  for(int i = 0; i < N; i++) {
-    A[i] = (int *)malloc(M*sizeof(int));
-  }
-  return A;
+    int **A = (int **) malloc(N * sizeof(int *));
+    for (int i = 0; i < N; i++) {
+        A[i] = (int *) malloc(M * sizeof(int));
+    }
+    return A;
 }
+
 void dynamic_array_free(int **A, int N)
 {
-  for(int i = 0; i < N; i++) {
-    free(A[i]);
-  }
-  free(A);
+    for (int i = 0; i < N; i++) {
+        free(A[i]);
+    }
+    free(A);
 }
--- library/commonUtils/numericOperations.h
+++ library/commonUtils/numericOperations.h
@@ -2,7 +2,11 @@
 #define SPBU_2020_C_NUMERICOPERATIONS_H
 
 int min(int a, int b);
+
 int max(int a, int b);
-int ** dynamic_array_alloc(int N, int M);
+
+int **dynamic_array_alloc(int N, int M);
+
 void dynamic_array_free(int **A, int N);
+
 #endif //SPBU_2020_C_NUMERICOPERATIONS_H
