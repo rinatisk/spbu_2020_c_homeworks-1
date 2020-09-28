11a5bf5c187710aa434ced9750114e30f6699f30
rinatisk <70975579+rinatisk@users.noreply.github.com>
2020-09-28 17:05:15 +0300

    fix naming and add func

---
+++ Homeworks_2/2_5sol_iteration_version.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int computeFactorialByIteration(int n)
+{
+    int currentFactorialValue = 1;
+    for (int i = 1; i < n + 1; ++i) {
+        currentFactorialValue *= i;
+    }
+    return currentFactorialValue;
+}
+
+int main()
+{
+    printf("Write n:\n");
+    int n = 0;
+    scanf("%d", &n);
+    n = computeFactorialByIteration(n);
+    printf("factorial n = %d", n, "\n");
+    return 0;
+}
--- Homeworks_2/2_5sol_recursion_version.c
+++ Homeworks_2/2_5sol_recursion_version.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-int computeFactorial(int n)
+int computeFactorialByRecursion(int n)
 {
     if (n <= 1) {
         return 1;
     }
-    return n * computeFactorial(n - 1);
+    return n * computeFactorialByRecursion(n - 1);
 }
 
 int main()
@@ -13,7 +13,7 @@ int main()
     printf("Write n:\n");
     int n = 0;
     scanf("%d", &n);
-    n = computeFactorial(n);
+    n = computeFactorialByRecursion(n);
     printf("factorial n = %d", n, "\n");
     return 0;
 }
