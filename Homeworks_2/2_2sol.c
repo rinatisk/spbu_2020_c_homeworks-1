68e3ac6c8a274b70a0d3dc94a68c0ba3bc073ff5
rinatisk <70975579+rinatisk@users.noreply.github.com>
2020-10-06 10:20:40 +0300

    Update 2_2sol.c

--- Homeworks_2/2_2sol.c
+++ Homeworks_2/2_2sol.c
@@ -1,40 +1,60 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
-const int tryLength = 4;
+const int digits = 10;
 
-int* getComputerGuess(int computerGuess[])
+int getUniqueNumber(bool* arrayOfDigits)
 {
+    int uniqueNumber = 0;
+    srand(time(NULL));
+    do {
+        uniqueNumber = rand() % 10;
+    } while (arrayOfDigits[uniqueNumber]);
+    return uniqueNumber;
+}
+
+int* getComputerGuess(int computerGuess[], int tryLength)
+{
+    bool* arrayOfDigits = malloc(digits * sizeof(bool));
+    memset(arrayOfDigits, false, digits);
     for (int i = 0; i < tryLength; ++i) {
-        computerGuess[i] = rand() % 6;
+        computerGuess[i] = getUniqueNumber(arrayOfDigits);
+        arrayOfDigits[computerGuess[i]] = true;
     }
+    free(arrayOfDigits);
     return computerGuess;
 }
 
-int* getUserTry(int userTry[])
+int* getUserTry(int userTry[], int tryLength)
 {
-    printf("Write you numbers throw a space\n");
+    printf("Write you numbers\n");
+    int n = 0;
+    scanf("%d", &n);
     for (int i = 0; i < tryLength; ++i) {
-        scanf("%d", &userTry[i]);
+        userTry[i] = n % 10;
+        n = n / 10;
     }
     return userTry;
 }
 
-bool isUserWin(int numberOfBulls)
+bool isUserWin(int numberOfBulls, int tryLength)
 {
     return numberOfBulls == tryLength;
 }
 
-void printGuessInfo(int numberOfBulls, int numberOfCows)
+void printGuessInfo(int numberOfBulls, int numberOfCows, int tryLength)
 {
-    if (isUserWin(numberOfBulls)) {
+    if (isUserWin(numberOfBulls, tryLength)) {
         printf("You win\n");
-    } else
+    }
+    else
         printf("Cows: %d Bulls: %d\n", numberOfCows, numberOfBulls);
 }
 
-int countCows(int computerGuess[], int userTry[])
+int countCows(int computerGuess[], int userTry[], int tryLength)
 {
     int numberOfCows = 0;
     for (int i = 0; i < tryLength; ++i) {
@@ -48,7 +68,7 @@ int countCows(int computerGuess[], int userTry[])
     return numberOfCows;
 }
 
-int countBulls(int computerGuess[], int userTry[])
+int countBulls(int computerGuess[], int userTry[], int tryLength)
 {
     int numberOfBulls = 0;
     for (int i = 0; i < tryLength; ++i) {
@@ -62,17 +82,21 @@ int countBulls(int computerGuess[], int userTry[])
 
 int main()
 {
-    int* computerGuess = calloc(tryLength, tryLength * sizeof(int));
-    int* userTry = calloc(tryLength, tryLength * sizeof(int));
-    computerGuess = getComputerGuess(computerGuess);
+    int tryLength = 0;
+    printf("write number length:\n");
+    scanf("%d", &tryLength);
+    int* computerGuess = calloc(tryLength, sizeof(int));
+    int* userTry = calloc(tryLength, sizeof(int));
+    computerGuess = getComputerGuess(computerGuess, tryLength);
     int numberOfBulls = 0;
     int numberOfCows = 0;
-    while (!isUserWin(numberOfBulls)) {
-        userTry = getUserTry(userTry);
-        numberOfBulls = countBulls(computerGuess, userTry);
-        numberOfCows = countCows(computerGuess, userTry);
-        printGuessInfo(numberOfBulls, numberOfCows);
+    while (!isUserWin(numberOfBulls, tryLength)) {
+        userTry = getUserTry(userTry, tryLength);
+        numberOfBulls = countBulls(computerGuess, userTry, tryLength);
+        numberOfCows = countCows(computerGuess, userTry, tryLength);
+        printGuessInfo(numberOfBulls, numberOfCows, tryLength);
     }
     free(computerGuess);
     free(userTry);
+    return 0;
 }
