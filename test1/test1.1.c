935ba68aef9f6b7ecafb7c7c08c58c7565f45328
rinatisk <70975579+rinatisk@users.noreply.github.com>
2020-10-13 11:59:25 +0300

    add 1 test

---
+++ test1/test1.1.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void mergeArrays(int firstArray[], int secondArray[], int sizeFirstArray, int sizeSecondArray, int mergedArray[])
+{
+    int i = 0;
+    int j = 0;
+    int k = 0;
+
+    while (i < sizeFirstArray && j < sizeSecondArray) {
+        if (firstArray[i] < secondArray[j]) {
+            mergedArray[k] = firstArray[i];
+            k++;
+            i++;
+        } else {
+            mergedArray[k] = secondArray[j];
+            k++;
+            j++;
+        }
+    }
+
+    while (i < sizeFirstArray) {
+        mergedArray[k] = firstArray[i];
+        k++;
+        i++;
+    }
+
+
+    while (j < sizeSecondArray) {
+        mergedArray[k] = secondArray[j];
+        k++;
+        j++;
+    }
+}
+
+
+int main()
+{
+    printf("write size of first array and elements of first array:\n");
+    int sizeFirstArray = 0;
+    scanf("%d", &sizeFirstArray);
+    int* firstArray = calloc(sizeFirstArray, sizeof(int));
+    for (int i = 0; i < sizeFirstArray; ++i) {
+        scanf("%d", &firstArray[i]);
+    }
+
+    printf("write size of second array and elements of second array:\n");
+    int sizeSecondArray = 0;
+    scanf("%d", &sizeSecondArray);
+    int* secondArray = calloc(sizeSecondArray, sizeof(int));
+    for (int i = 0; i < sizeSecondArray; ++i) {
+        scanf("%d", &secondArray[i]);
+    }
+
+    int* mergedArray = calloc(sizeFirstArray + sizeSecondArray, sizeof(int));
+
+    mergeArrays(firstArray, secondArray, sizeFirstArray, sizeSecondArray, mergedArray);
+
+    printf("Array after merging\n");
+    for (int i = sizeFirstArray + sizeSecondArray - 1; i >= 0; --i)
+        printf("%d ", mergedArray[i]);
+
+    return 0;
+} 
