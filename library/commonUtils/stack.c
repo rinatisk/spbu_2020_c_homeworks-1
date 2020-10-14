43cf1654ce27d54c7f45b5176c753fe3ef19d822
rinatisk <70975579+rinatisk@users.noreply.github.com>
2020-10-14 09:59:14 +0300

    fix style

--- library/commonUtils/stack.c
+++ library/commonUtils/stack.c
@@ -25,46 +25,51 @@ StackElement* createStackElement(int storeElement)
     url->next = NULL;
     return url;
 }
-void push(Stack* stack, StackElement* storeElement) {
-    storeElement->next=stack->top;
-    stack->top=storeElement;
+
+void push(Stack* stack, StackElement* storeElement)
+{
+    storeElement->next = stack->top;
+    stack->top = storeElement;
     stack->size++;
 }
+
 bool isEmpty(Stack* stack)
 {
-    return stack->size==0;
+    return stack->size == 0;
 }
+
 StackElement* pop(Stack* stack)
 {
-    if(isEmpty(stack))
-    {
+    if (isEmpty(stack)) {
         return NULL;
     }
-    StackElement* element=stack->top;
-    stack->top=element->next;
-    element->next=NULL;
+    StackElement* element = stack->top;
+    stack->top = element->next;
+    element->next = NULL;
     stack->size--;
     return element;
 }
+
 void deleteStackElement(StackElement* stackElement)
 {
     free(stackElement);
 }
+
 void deleteStack(Stack* stack)
 {
-    while(!isEmpty(stack))
-    {
-        StackElement* element=pop(stack);
+    while (!isEmpty(stack)) {
+        StackElement* element = pop(stack);
         deleteStackElement(element);
     }
     free(stack);
 }
+
 int getSize(Stack* stack)
 {
     return stack->size;
 }
 
-int getValue (StackElement* stackElement)
+int getValue(StackElement* stackElement)
 {
     return stackElement->storeElement;
 }
--- library/commonUtils/stack.h
+++ library/commonUtils/stack.h
@@ -1,19 +1,27 @@
 #ifndef INC_2909PRACT__LIST_H_
 #define INC_2909PRACT__LIST_H_
+#include <stdbool.h>
 
 typedef struct StackElement StackElement;
 
 typedef struct Stack Stack;
-#include <stdbool.h>
+
 Stack* createStack();
 
 StackElement* createStackElement(int store);
 
 void push(Stack* stack, StackElement* newElement);
-int getSize (Stack* stack);
+
+int getSize(Stack* stack);
+
 StackElement* pop(Stack* stack);
+
 bool isEmpty(Stack* stack);
+
 void deleteStackElement(StackElement* stackElement);
+
 void deleteStack(Stack* stack);
-int getValue (StackElement* stackElement);
+
+int getValue(StackElement* stackElement);
+
 #endif//INC_2909PRACT__LIST_H_
